d9b517a1efe882d4a2bd8da0bc22214a59b6fae2
Alain <alain.guineaja@elorrieta-errekamari.com>
2025-10-31 15:51:47 +0100

    Creación de la clase UserController

--- app/src/main/java/com/example/reto1movilesgrupo2/controllers/UserController.kt
+++ app/src/main/java/com/example/reto1movilesgrupo2/controllers/UserController.kt
@@ -6,17 +6,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.reto1movilesgrupo2.R
+import com.example.reto1movilesgrupo2.entities.User
+import com.example.reto1movilesgrupo2.firebase.ManagerFactory
 
 class UserController : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    public fun selectAll(): List<User> {
-        List<User> users = new ArrayList<User>();
+    public fun selectAll(): MutableList<User> {
+        //var users: MutableList<User> = mutableListOf()
+        return ManagerFactory().getUserManager().selectAll()
+    }
 
-        ManagerFactory
+    public fun insert(newUser: User) {
+        ManagerFactory().getUserManager().insert(newUser)
+    }
 
-        return users;
+    public fun update(userToUpdate: User) {
+        ManagerFactory().getUserManager().update(userToUpdate)
+    }
+
+    public fun delete(userToDelete: User) {
+        ManagerFactory().getUserManager().delete(userToDelete)
     }
 }
