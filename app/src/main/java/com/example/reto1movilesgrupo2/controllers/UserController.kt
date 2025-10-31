73d442f3ec02d748ead6f65053853b87330c7ba8
Alain <alain.guineaja@elorrieta-errekamari.com>
2025-10-31 15:35:14 +0100

    Creación de la clase UserController

---
+++ app/src/main/java/com/example/reto1movilesgrupo2/controllers/UserController.kt
@@ -0,0 +1,22 @@
+package com.example.reto1movilesgrupo2.controllers
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.reto1movilesgrupo2.R
+
+class UserController : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    public fun selectAll(): List<User> {
+        List<User> users = new ArrayList<User>();
+
+        ManagerFactory
+
+        return users;
+    }
+}
