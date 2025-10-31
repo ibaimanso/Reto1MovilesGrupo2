bccabf8dbc33d8b2b58563cc964988ad259ed4a0
Alain <alain.guineaja@elorrieta-errekamari.com>
2025-10-31 21:11:50 +0100

    Avances en el RegistroActivity

--- app/src/main/java/com/example/reto1movilesgrupo2/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/reto1movilesgrupo2/activities/RegistroActivity.kt
@@ -1,13 +1,31 @@
 package com.example.reto1movilesgrupo2.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.reto1movilesgrupo2.R
+import com.example.reto1movilesgrupo2.entities.User
+import kotlinx.coroutines.launch
 
 class RegistroActivity : AppCompatActivity() {
+    private lateinit var logo:        ImageView
+    private lateinit var inputFname:  EditText
+    private lateinit var inputLname:  EditText
+    private lateinit var inputPw:     EditText
+    private lateinit var inputEmail:  EditText
+    private lateinit var inputBirth:  EditText
+    private lateinit var inputType:   Spinner
+    private lateinit var btnRegister: Button
+    private lateinit var btnGoBack:   Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,7 +36,40 @@ class RegistroActivity : AppCompatActivity() {
             insets
         }
 
+        logo = findViewById(R.id.appLogo)
+        logo.setImageResource(R.mipmap.iconogym)
 
+        inputFname = findViewById(R.id.inputName)
+        inputLname = findViewById(R.id.inputLName)
+        inputPw    = findViewById(R.id.inputPassword)
+        inputEmail = findViewById(R.id.inputEmail)
+        inputBirth = findViewById(R.id.inputBirth)
+        inputType  = findViewById(R.id.userType)
 
+        btnRegister = findViewById(R.id.btnRegister)
+        btnRegister.setOnClickListener {
+            lifecycleScope.launch {
+                register()
+            }
+        }
+
+        btnGoBack = findViewById(R.id.btnBack)
+        btnGoBack.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun register() {
+        val user = User()
+
+        user.fname = inputFname.text.toString()
+        user.lname = inputLname.text.toString()
+        user.pw    = inputPw.text.toString()
+        user.email = inputEmail.text.toString()
+        user.birth = inputBirth.text.toString()
+
+        // #TODO
     }
 }
