9aeb25d3b27ed72948b5e63acc0ec5c0de11c0ba
haritz <haritz.rodriguezro@elorrieta-errekamari.com>
2025-11-04 17:45:45 +0100

    Establezco utilidad a todos los botones además de mostrar los workouts

--- app/src/main/java/com/example/reto1movilesgrupo2/activities/WorkoutsActivity.kt
+++ app/src/main/java/com/example/reto1movilesgrupo2/activities/WorkoutsActivity.kt
@@ -1,10 +1,16 @@
 package com.example.reto1movilesgrupo2.activities
 
 import android.content.Intent
+import android.graphics.Color
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,20 +18,29 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.reto1movilesgrupo2.R
+import com.example.reto1movilesgrupo2.controllers.ControllerFactory
+import com.example.reto1movilesgrupo2.entities.User
+import com.example.reto1movilesgrupo2.entities.Workout
+import com.example.reto1movilesgrupo2.firebase.ManagerFactory
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 class WorkoutsActivity : AppCompatActivity() {
 
     private lateinit var btnProfile: Button
-    private lateinit var btnBack:    Button
-    private lateinit var btnFilter:  Button
+    private lateinit var btnBack: Button
+    private lateinit var btnFilter: Button
     private lateinit var btnTrainer: Button
+
+    private lateinit var workoutTable: TableLayout
 
     private lateinit var logo: ImageView
 
     private lateinit var userLevel: TextView
 
     private lateinit var inputLevelFilter: EditText
+
+    private lateinit var userName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,13 +56,22 @@ class WorkoutsActivity : AppCompatActivity() {
         logo.setImageResource(R.mipmap.iconogym)
 
         btnProfile = findViewById(R.id.btnProfile)
-        btnBack    = findViewById(R.id.btnBack)
-        btnFilter  = findViewById(R.id.btnFilter)
+        btnBack = findViewById(R.id.btnBack)
+        btnFilter = findViewById(R.id.btnFilter)
         btnTrainer = findViewById(R.id.btnTrainer)
 
-        val userName = intent.getStringExtra("USERFNAME")
+        workoutTable = findViewById(R.id.workoutTable)
 
-        // Tenemos el usuario, hay que comprobar si es entrenador o no y mostrar o no el botón entrenador
+        userName = intent.getStringExtra("USERFNAME").toString()
+
+        btnTrainer.visibility = Button.GONE
+
+        lifecycleScope.launch {
+            val workoutsByLevel: MutableList<Workout>? = loadWorkoutsByLevel()
+            insertIntoTable(workoutsByLevel)
+
+            showTrainerButtonIfNeeded()
+        }
 
 
         btnBack.setOnClickListener {
@@ -55,8 +79,15 @@ class WorkoutsActivity : AppCompatActivity() {
         }
 
         btnTrainer.setOnClickListener {
-            goToTrainer()
+            lifecycleScope.launch {
+                goToTrainer()
+            }
         }
+
+        btnProfile.setOnClickListener {
+            goToProfile()
+        }
+
     }
 
     private fun goBack() {
@@ -71,6 +102,78 @@ class WorkoutsActivity : AppCompatActivity() {
         finish()
     }
 
+    private fun goToProfile() {
+        val intent = Intent(this, PerfilActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
+    suspend fun loadWorkoutsByLevel(): MutableList<Workout>? {
+        val tempUser = User()
+        tempUser.fname = userName
 
+        var user: User? =
+            ControllerFactory.getInstance()?.getUserController()?.selectByFname(tempUser)
+        if (user == null) user = User()
+
+        val workouts: MutableList<Workout>? =
+            ControllerFactory.getInstance()?.getWorkoutController()?.selectByLevel(user)
+
+        return workouts
+
+    }
+
+    suspend fun showTrainerButtonIfNeeded() {
+        val tempUser = User()
+        tempUser.fname = userName
+
+        var user: User? =
+            ControllerFactory.getInstance()?.getUserController()?.selectByFname(tempUser)
+        if (user == null) user = User()
+
+        if (user.trainer) {
+            btnTrainer.visibility = Button.VISIBLE
+        }
+    }
+
+    suspend fun insertIntoTable(workouts: MutableList<Workout>?) {
+        val dateFormat = SimpleDateFormat("yyyy-mm-dd", Locale.getDefault())
+
+        if (workouts == null) {
+            return
+        }
+
+        for (workout: Workout in workouts) {
+            val row = TableRow(this)
+
+            fun createCell(texto: String?): TextView {
+                return TextView(this).apply {
+                    text = texto
+                    gravity = Gravity.CENTER
+                    setPadding(16, 12, 16, 12)
+                    maxLines = 1
+                    ellipsize = TextUtils.TruncateAt.END
+                }
+            }
+
+            // Crear las celdas
+            val nameView = createCell(workout.name)
+            val levelView = createCell(workout.level.toString())
+            val timeView = createCell("${ControllerFactory().getWorkoutController().getExpectedTime(workout)} min")
+            val dateView = createCell("")
+            val completedView = createCell("")
+            val videoView = createCell(workout.videoUrl)
+
+            // Añadir las celdas a la fila
+            row.addView(nameView)
+            row.addView(levelView)
+            row.addView(timeView)
+            row.addView(dateView)
+            row.addView(completedView)
+            row.addView(videoView)
+
+            // Agregar la fila a la tabla
+            workoutTable.addView(row)
+        }
+    }
 }
