752842aad8e94a684bcb56ad264c061a1408f43d
haritz <haritz.rodriguezro@elorrieta-errekamari.com>
2025-11-04 17:44:02 +0100

    Creación de selectByLevel y getExpectedTime en el WorkoutController

--- app/src/main/java/com/example/reto1movilesgrupo2/controllers/WorkoutController.kt
+++ app/src/main/java/com/example/reto1movilesgrupo2/controllers/WorkoutController.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.reto1movilesgrupo2.R
+import com.example.reto1movilesgrupo2.entities.Exercise
 import com.example.reto1movilesgrupo2.entities.User
 import com.example.reto1movilesgrupo2.entities.Workout
 import com.example.reto1movilesgrupo2.firebase.ManagerFactory
@@ -30,4 +31,32 @@ class WorkoutController : AppCompatActivity() {
     suspend fun delete(workoutToDelete: Workout) {
         ManagerFactory().getWorkoutManager().delete(workoutToDelete)
     }
+
+    suspend fun selectByLevel(userLogged: User): MutableList<Workout> {
+        val ret = mutableListOf<Workout>()
+        val workouts: MutableList<Workout> = selectAll()
+
+        for (workout: Workout in workouts) {
+            if (workout.level <= userLogged.level) {
+                ret.add(workout)
+            }
+        }
+
+        return ret
+    }
+
+    suspend fun getExpectedTime(workout: Workout): Int {
+        val exercises: MutableList<Exercise> = ControllerFactory().getExerciseController().selectAllByWorkout(workout)
+
+        var totalTime: Int = 0
+
+        for (exercise: Exercise in exercises) {
+
+            totalTime += ControllerFactory().getExerciseController().getExpectedTime(exercise)
+
+        }
+
+        return totalTime
+    }
+
 }
